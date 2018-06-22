d85dc0a7cbcfe10be06404bd0bff18f031282be7
tuyennguyen995 <tuyennguyen995@gmail.com>
2018-06-22 16:03:38 +0700

    Database online

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ app.use(Passport.session());
 //Kết nối database
 const sequelize = require('sequelize');
 const config = new sequelize ({
-  host: "localhost",
-  username: "postgres",
-  password: "admin@123",
-  database: "DataNTT",
+  host: "ec2-54-204-23-228.compute-1.amazonaws.com",
+  username: "qsazzvvbopiolj",
+  password: "23d411af50c296de532da9df874d68d657d9af9108a9607baa3e0d1cde1931ef",
+  database: "dauaa88394fkno",
   port:"5432",
   dialect: "postgres",
   operatorsAliases: false,
