eae49974a8b886e8acdfc4a24f0af68fbed82e6d
tuyennguyen995 <tuyennguyen995@gmail.com>
2018-06-24 19:25:09 +0700

    up

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const session = require('express-session');
 const LocalStrategy = require('passport-local').Strategy;
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(http);
 server.listen(process.env.PORT || 3000);
 
 app.set('views', './views');
