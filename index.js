17ff81b0f0b21570595ca80cd9fdaa9744f57133
tuyennguyen995 <tuyennguyen995@gmail.com>
2018-07-01 17:57:18 +0700

    up

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const TRANGTHAI = config.define('TRANGTHAI',{
   TEN_TRANGTHAI: sequelize.STRING,
   APDUNG: sequelize.STRING
 })
- 
+
 const TINH = config.define('TINH',{
   TEN_TINH: sequelize.STRING
 })
@@ -57,7 +57,7 @@ const PHUONG_XA = config.define('PHUONG_XA',{
 const INFO_USER = config.define('INFO_USER',{
   HOTEN: sequelize.STRING,
   GIOITINH: sequelize.STRING,
-  NGAYSINH: sequelize.TEXT,
+  NGAYSINH: sequelize.DATE,
   CMND: sequelize.STRING,
   SDT: sequelize.STRING,
   MAIL: sequelize.STRING,
@@ -97,7 +97,7 @@ app.get('/admin/edit/:id',function(req, res){
   var gioitinhs = "";
      if(req.isAuthenticated()){
       var i = req.params.id;
-      config.query('SELECT * FROM public."USER" as a, public."INFO_USER" as b, public."TRANGTHAI" as c, public."COQUAN" as d, public."PHUONG_XA" as e, public."QUAN_HUYEN" as f, public."TINH" as g where a.info_user = b.id and a.trangthai = c.id and a.coquan = d.id and d.ma_diachi = e.id and e.ma_quan_huyen = f.id and f.ma_tinh = g.id and a.id ='+ i)
+      config.query('SELECT * FROM public."INFO_USER" as b, public."TRANGTHAI" as c, public."COQUAN" as d, public."PHUONG_XA" as e, public."QUAN_HUYEN" as f, public."TINH" as g, public."USER" as a where a.info_user = b.id and a.trangthai = c.id and a.coquan = d.id and d.ma_diachi = e.id and e.ma_quan_huyen = f.id and f.ma_tinh = g.id and a.id ='+ i)
       .then(arr => {
         TINH.findAll({where:{id: {[Op.ne]:  arr[0][0].ma_tinh}}})
         .then(arrTinh =>{
@@ -110,7 +110,16 @@ app.get('/admin/edit/:id',function(req, res){
               }else {
                 gioitinhs = "Nam";
               }
-              res.render("admin/edit.ejs",{data1: arr, data2: arrqh, data3: arrpx,data4: arrTinh, data: req.user, gt: gioitinhs});
+              config.query('SELECT to_char("NGAYCAP",'+ "'yyyy-MM-dd'"+') FROM public."INFO_USER" where id ='+arr[0][0].info_user)
+              .then(ncap =>{
+                config.query('SELECT to_char("NGAYSINH",'+ "'yyyy-MM-dd'"+') FROM public."INFO_USER" where id ='+arr[0][0].info_user)
+                .then(nsinh =>{
+                  TRANGTHAI.findAll({where:{[Op.and]: [{id: {[Op.ne]:  arr[0][0].trangthai}},{APDUNG: "USER"}]}})
+                  .then(tthai =>{
+                    res.render("admin/edit.ejs",{data1: arr, data2: arrqh, data3: arrpx,data4: arrTinh, data: req.user, gt: gioitinhs, nc: ncap, ns: nsinh, tt: tthai});
+                  })
+                })
+              })
             })
           })
         })
@@ -138,24 +147,98 @@ app.post('/admin/edit/:id',urlencodedParser, function(req, res){
   ngaysinh = req.body.ngaysinh;
   gioitinh = req.body.gioitinh;
   hoten = req.body.hoten;
+  tthai = req.body.tthai;
 
-  USER.update({
-    username: user,
-    password: pass
-  },{
-    where: {id: i}
-  }).then(row => {
-    res.redirect('/admin')
+  var us = -1;
+  USER.findOne({where:{id: i}})
+  .then(sUSER => {
+    if(user != sUSER.username){ //Kiểm ten username đã có thay đổi không
+      USER.findAll({where:{username: user}})  //Kiểm tra thay username có tồn tại chưa
+      .then(sUSER => {
+        if(sUSER.length > 0){
+          us = 1
+        }else {
+          us = 0;
+          temp = -2;  //Lỗi tên user đã tồn tại
+        }
+      })
+      if(us == 1){
+        res.redirect('/admin/edit/'+i);
+      }else {
+        USER.update({
+          username: user,
+          password: pass,
+          trangthai: tthai
+        },{
+          where: {id: i}
+        });
+        COQUAN.update({
+          MA_COQUAN: macoquan,
+          TEN_COQUAN: coquan,
+          BOPHAN: bophan,
+          DIACHI_SO: diachi,
+          ma_diachi: phuong
+        },{
+          where: {id: sUSER.coquan}
+        });
+        INFO_USER.update({
+          HOTEN: hoten,
+          GIOITINH: gioitinh,
+          NGAYSINH: ngaysinh,
+          CMND: cmnd,
+          SDT: sdt,
+          MAIL: mail,
+          NGAYCAP: ngaycap,
+          NOICAP: noicap
+        },{
+          where: {id: sUSER.info_user}
+        });
+        res.redirect('/admin');
+      }
+    }else {
+      USER.update({
+        username: user,
+        password: pass,
+        trangthai: tthai
+      },{
+        where: {id: i}
+      });
+      COQUAN.update({
+        MA_COQUAN: macoquan,
+        TEN_COQUAN: coquan,
+        BOPHAN: bophan,
+        DIACHI_SO: diachi,
+        ma_diachi: phuong
+      },{
+        where: {id: sUSER.coquan}
+      });
+      INFO_USER.update({
+        HOTEN: hoten,
+        GIOITINH: gioitinh,
+        NGAYSINH: ngaysinh,
+        CMND: cmnd,
+        SDT: sdt,
+        MAIL: mail,
+        NGAYCAP: ngaycap,
+        NOICAP: noicap
+      },{
+        where: {id: sUSER.info_user}
+      });
+      res.redirect('/admin');
+      }
   })
 })
 
 app.get('/admin/delete/:id',function(req, res){
     if(req.isAuthenticated()){
       var i = req.params.id;
-      USER.destroy({where: {id: i}})
-      .then(row =>{
-        res.redirect('/admin');
+      USER.findOne({where: {id: i}})
+      .then(user =>{
+        COQUAN.destroy({where: {id: user.coquan}});
+        INFO_USER.destroy({where: {id: user.info_user}});
       })
+      USER.destroy({where: {id: i}});
+      res.redirect('/admin');
     }else {
       res.redirect('/login');
     }
@@ -200,7 +283,7 @@ app.post('/admin/create',urlencodedParser, function(req, res){
 
     }else{
       us = 0;
-      temp = -2;
+      temp = -2;  //Lỗi tên user đã tồn tại
     }
   })
   if(us == 0){
@@ -249,17 +332,23 @@ Passport.use(new LocalStrategy((username, password, done) => {
   USER.findOne({where:{username: username}})
   .then(USER => {
     //code
-    if(USER.password == password){
-      console.log("Password chính xác!");
-      return done(null, USER.username);
+    if(USER.trangthai == 1){
+      if(USER.password == password){
+        console.log("Password chính xác!");
+        return done(null, USER.username);
+      }else {
+          temp = -1; //Lỗi mật khẩu không chính xác
+          console.log("Password không chính xác!");
+          return done(null, false);
+      }
     }else {
-        temp = -1;
-        console.log("Password không chính xác!");
-        return done(null, false);
+      temp = -3; //Lỗi tài khoản bị khóa
+      console.log("Tài khoản đã bị khóa!");
+      return done(null, false);
     }
   })
   .catch(err => {
-    temp = 1;
+    temp = 1; //Lỗi tài khoản không tồn tại
     console.log('Tài khoản không tồn tại!');
     return done(null, false);
   })
